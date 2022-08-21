4195c71995b4f8702e9bca8867d0ca76d93ab13e
Daiana Rodrigues Nascimento <102737592+DaianaRodrigues@users.noreply.github.com>
2022-08-20 22:19:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'criançamas.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovemmas.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adultomas.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idosomas.png')
+            }
+        } else if (fsex[1].checked){
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10){
+                //Criança
+                img.setAttribute('src', 'criancafem.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovemfem.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adultafem.png')
+            } else {
+                //idosa
+                img.setAttribute('src', 'idosafem.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
+    }
+}
